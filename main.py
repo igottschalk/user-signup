c4782de2b8d8cf44cbf792681a4b7daa7c747106
Ingrid Gottschalk <igottschalk@charter.net>
2018-04-04 19:03:28 -0500

    created the names and fields in the form

---
+++ main.py
@@ -0,0 +1,68 @@
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+app.config['DEBUG'] = True
+
+
+form = """
+<!DOCTYPE html>
+<html>
+    <head> 
+        <title>Signup</title>           
+    </head>
+   <body>
+        <h1>Signup</h1>
+        <form method="post">
+            <table>
+                <tbody>
+                    <tr>
+                        <td>
+                            <label for="username">Username</label>
+                        </td>
+                        <td>
+                            <input name="username" value="" type="text">
+                            <span class="error"></span>
+                        </td>
+                    </tr>
+                     <tr>
+                        <td>
+                            <label for="password">Password</label>
+                        </td>
+                        <td>
+                            <input name="password" value="" type="password">
+                            <span class="error"></span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label for="verify">Verify Password</label>
+                        </td>
+                        <td>
+                            <input name="verify" value="" type="password">
+                            <span class="error"></span>
+                        </td>
+                    </tr>
+                     <tr>
+                        <td>
+                            <label for="email">Email (optional)</label>
+                        </td>
+                        <td>
+                            <input name="email" value="">
+                            <span class="error"></span>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <input type="submit">
+        </form>
+    </body>
+</html> 
+"""
+
+@app.route("/")
+def index():
+    return form
+
+app.run()
